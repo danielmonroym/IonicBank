a977249931108bcfab78eb55858c8b8f48a9c546
mBaenaCh <mateo.baena@hotmail.com>
2020-11-08 14:45:46 -0500

    Adding the login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
   
 
 @NgModule({
   declarations: [ AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,

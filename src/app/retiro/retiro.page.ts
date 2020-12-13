719532d27cff5f765aa99335638895eae38e76f0
daniel.monroy <daniel.monroym@udea.edu.co>
2020-12-13 16:33:35 -0500

    added logic retiro

--- src/app/retiro/retiro.page.ts
+++ src/app/retiro/retiro.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TokenService } from '../services/token.service';
+import jwt_decode   from 'jwt-decode';
 @Component({
   selector: 'app-retiro',
   templateUrl: './retiro.page.html',
   styleUrls: ['./retiro.page.scss'],
 })
 export class RetiroPage implements OnInit {
+  tokenValidation: any;
+  decodedToken:any;
+  input1:any;
+  input2:any;
+  input3:number;
+  input4:any;
 
-  constructor() { }
+
+  constructor(private tokenService:TokenService) { }
 
   ngOnInit() {
+    this.tokenValidation=this.tokenService.getToken();
+    this.decodedToken= jwt_decode(this.tokenValidation);
+    console.log(this.decodedToken);
+    this.input1=this.decodedToken.ownerName;
+    console.log(this.input1);
+    this.input2=this.decodedToken.ownerId;
+    this.input4=this.decodedToken.ownerAccounts[0].accountId;
+
   }
 
 }

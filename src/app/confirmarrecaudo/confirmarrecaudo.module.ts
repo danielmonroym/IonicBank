0588b334917724046e4e575bf1c505cf8f29faff
mBaenaCh <mateo.baena@hotmail.com>
2020-12-14 15:27:01 -0500

    Recaudos feature is now addedd

--- src/app/confirmarrecaudo/confirmarrecaudo.module.ts
+++ src/app/confirmarrecaudo/confirmarrecaudo.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { ConfirmarrecaudoPageRoutingModule } from './confirmarrecaudo-routing.module';
 
 import { ConfirmarrecaudoPage } from './confirmarrecaudo.page';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { ImagenComponent } from '../imagen/imagen.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ConfirmarrecaudoPageRoutingModule
+    ConfirmarrecaudoPageRoutingModule,
+    NgxQRCodeModule
   ],
-  declarations: [ConfirmarrecaudoPage]
+  declarations: [ConfirmarrecaudoPage, ImagenComponent]
 })
 export class ConfirmarrecaudoPageModule {}
--- src/app/recaudos/recaudos.page.ts
+++ src/app/recaudos/recaudos.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+import { TokenService } from '../services/token.service';
+import jwt_decode from 'jwt-decode';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-recaudos',
@@ -6,10 +11,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recaudos.page.scss'],
 })
 export class RecaudosPage implements OnInit {
+  tokenValidation: any;
+  decodedToken: any;
+  input1: any; //Codigo convenio
+  input2: any; //Codigo factura
+  input3: any; //Nombre titular
+  input4: any; //Documento
+  input5: any; //Monto
+  input6: any; //Cuentas del titular
+  selectedPaymentValue: any; //Tipo de pago seleccionado
+  selectedAccountValue: any; //Tipo de cuenta seleccionada
+  showBadge: boolean;
+  montoDisabled: boolean;
+  activateAccountSelector: boolean;
+  limitMoneyAmmount = 3000000;
 
-  constructor() { }
+  constructor(private tokenService: TokenService, public router: Router, public dataService: DataService) { }
 
   ngOnInit() {
+    this.tokenValidation = this.tokenService.getToken();
+    this.decodedToken = jwt_decode(this.tokenValidation);
+    this.input3 = this.decodedToken.ownerName;
+    this.input4 = this.decodedToken.ownerId;
+    this.input6 = this.decodedToken.ownerAccounts;
+    if (this.selectedPaymentValue == undefined) {
+      this.montoDisabled = true;
+      this.activateAccountSelector = false;
+    }
+  }
+
+  verifyBalance(): void {
+    if (this.activateAccountSelector) {
+      if (this.input5 >= this.selectedPaymentValue || this.input5 >= this.decodedToken.maxTransferAmount) {
+        this.showBadge = true;
+      } else {
+        this.showBadge = false;
+      }
+    } else if (this.input5 >= this.limitMoneyAmmount) {
+      this.showBadge = true;
+    } else {
+      this.showBadge = false;
+    }
+  }
+
+  changeSelected(): void {
+    this.montoDisabled = false;
+    if (this.selectedPaymentValue === "Tarjeta") {
+      this.activateAccountSelector = true;
+      console.log(this.selectedPaymentValue);
+    } else {
+      this.activateAccountSelector = false;
+    }
+
+  }
+
+  getQRData() {
+    let dataObject = {};
+    if (this.activateAccountSelector) {
+      dataObject = {
+        qrData1: this.input1,
+        qrData2: this.input2,
+        qrData3: this.input3,
+        qrData4: this.input4,
+        qrData5: this.selectedPaymentValue.accountType,
+        qrData6: this.selectedAccountValue,
+        qrData7: this.input5
+      }
+
+    } else {
+      dataObject = {
+        qrData1: this.input1,
+        qrData2: this.input2,
+        qrData3: this.input3,
+        qrData4: this.input4,
+        qrData5: this.selectedPaymentValue,
+        qrData6: this.input5
+      }
+    }
+    console.log(dataObject);
+    this.dataService.setParamData(dataObject);
+    this.router.navigateByUrl('confirmarrecaudo')
   }
 
 }
